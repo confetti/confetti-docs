f853871844035512e59b548c04f6d69d2798a229
Jonny Strömberg <jonny.stromberg@gmail.com>
2023-11-15 16:19:50 +0100

    Fix proces/browser to work with build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,7 +1,7 @@
 import { themes as prismThemes } from 'prism-react-renderer'
 import type { Config } from '@docusaurus/types'
 import type * as Preset from '@docusaurus/preset-classic'
-import webpack from 'webpack'
+const { ProvidePlugin } = require('webpack')
 
 const config: Config = {
   title: 'Confetti API Documentation',
@@ -137,8 +137,8 @@ const config: Config = {
             },
           },
           plugins: [
-            new webpack.ProvidePlugin({
-              process: 'process/browser',
+            new ProvidePlugin({
+              process: require.resolve('process/browser'),
             }),
           ],
         }

c9b378a76f1f0a2b58453c57c883d64a237170ae
borkanee <grubesic.boris@gmail.com>
2023-11-20 18:05:32 +0100

    adds contacts find, findAll and POST for tickets and contacts

--- src/components/Parameters.tsx
+++ src/components/Parameters.tsx
@@ -1,8 +1,8 @@
-function HeaderRow() {
+function HeaderRow({ isCreateOrUpdate }: { isCreateOrUpdate?: boolean }) {
   return (
     <thead>
       <th>Parameter</th>
-      <th>Default</th>
+      <th>{isCreateOrUpdate ? 'Type' : 'Default'}</th>
       <th>Values / Description</th>
     </thead>
   )
@@ -51,6 +51,35 @@ const queryDefaultValue = (filter) => {
   }
 }
 
+function CreateOrUpdateParams({ attributes }: { attributes: any[] }) {
+  if (!attributes || attributes.length === 0) return null
+
+  return attributes.map((attr: any) => {
+    const description = (() => {
+      if (attr.values) {
+        return attr.values.map((value: any) => (
+          <>
+            <code>{value}</code>{' '}
+          </>
+        ))
+      } else {
+        return attr.helpText
+      }
+    })()
+
+    const type = (() => {
+      if (attr.multiple) {
+        return `array of ${attr.type}s`
+      } else {
+        return attr.type
+      }
+    })()
+
+    const name = `${attr.key}${attr.required ? '*' : ''}`
+    return <Row name={name} defaultValue={type} description={description} />
+  })
+}
+
 function Filters({ filters }: { filters: any }) {
   if (!filters || Object.keys(filters).length === 0) return null
   return (
@@ -75,34 +104,46 @@ function Filters({ filters }: { filters: any }) {
 export default function Parameters({
   model,
   includeOnly,
+  isCreateOrUpdate,
 }: {
   model: any
   includeOnly?: boolean
+  isCreateOrUpdate?: boolean
 }) {
   return (
     <table>
-      <HeaderRow />
-      {!includeOnly && (
+      <HeaderRow isCreateOrUpdate={isCreateOrUpdate} />
+      {isCreateOrUpdate ? (
+        <CreateOrUpdateParams attributes={model.operations.create.attributes} />
+      ) : (
         <>
-          <Filters filters={model.filters} />
-          <Row
-            name="page[size]"
-            defaultValue="50"
-            description="Maximum number of results"
-          />
-          <Row name="page[number]" defaultValue="1" description="Page number" />
-        </>
-      )}
-      {model.includes && (
-        <Row
-          name="include"
-          defaultValue=""
-          description={model.includes.map((i: string) => (
+          {!includeOnly && (
             <>
-              <code>{i}</code>{' '}
+              <Filters filters={model.filters} />
+              <Row
+                name="page[size]"
+                defaultValue="50"
+                description="Maximum number of results"
+              />
+              <Row
+                name="page[number]"
+                defaultValue="1"
+                description="Page number"
+              />
             </>
-          ))}
-        />
+          )}
+          {model.includes && (
+            <Row
+              name="include"
+              defaultValue=""
+              description={model.includes.map((i: string) => (
+                <>
+                  <code>{i}</code>{' '}
+                </>
+              ))}
+            />
+          )}
+        </>
       )}
     </table>
   )
